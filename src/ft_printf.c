91357bfb040d9743270db3fb8292428b8e37f6cb
Torrenté Florian <florian@deneria.net>
2023-10-29 23:21:34 +0100

    Fixed norminette and Makefile

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: fltorren <fltorren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 09:44:01 by fltorren          #+#    #+#             */
-/*   Updated: 2023/10/29 23:12:00 by fltorren         ###   ########.fr       */
+/*   Updated: 2023/10/29 23:19:15 by fltorren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,36 +55,46 @@ int	ft_printf(const char *format, ...)
 	va_end(args);
 	return (len + i);
 }
-
-#include <stdio.h>
-int	main(void)
-{
-	int len = ft_printf("Hello, %s!\n", "world");
-	int rlen = printf("Hello, %s!\n", "world");
-	printf("len = %d, rlen = %d\n", len, rlen);
-
-	len = ft_printf("Hello, %c%c%%!\n", 'w', '\0');
-	rlen = printf("Hello, %c%c%%!\n", 'w', '\0');
-	printf("len = %d, rlen = %d\n", len, rlen);
-
-	len = ft_printf("Hello, %d, %d, %d, %d, %d!\n", 42, 0, 0, INT32_MAX, INT32_MIN);
-	rlen = printf("Hello, %d, %d, %d, %d, %d!\n", 42, 0, 0, INT32_MAX, INT32_MIN);
-	printf("len = %d, rlen = %d\n", len, rlen);
-
-	len = ft_printf("Hello, %u, %u, %u, %u, %u!\n", 42, 0, 0, UINT32_MAX, UINT32_MAX + 1);
-	rlen = printf("Hello, %u, %u, %u, %u, %u!\n", 42, 0, 0, UINT32_MAX, UINT32_MAX + 1);
-	printf("len = %d, rlen = %d\n", len, rlen);
-
-	len = ft_printf("Hello, %x, %x, %x, %x, %x!\n", 42, 0, 0, UINT32_MAX, UINT32_MAX + 1);
-	rlen = printf("Hello, %x, %x, %x, %x, %x!\n", 42, 0, 0, UINT32_MAX, UINT32_MAX + 1);
-	printf("len = %d, rlen = %d\n", len, rlen);
-
-	len = ft_printf("Hello, %X, %X, %X, %X, %X!\n", 42, 0, 0, UINT32_MAX, UINT32_MAX + 1);
-	rlen = printf("Hello, %X, %X, %X, %X, %X!\n", 42, 0, 0, UINT32_MAX, UINT32_MAX + 1);
-	printf("len = %d, rlen = %d\n", len, rlen);
-
-	len = ft_printf("Hello, %p, %p, %p!\n", &len, &rlen, NULL);
-	rlen = printf("Hello, %p, %p, %p!\n", &len, &rlen, NULL);
-	printf("len = %d, rlen = %d\n", len, rlen);
-	return (0);
-}
+/*
+**
+** #include <stdio.h>
+** int	main(void)
+** {
+** 	int len = ft_printf("Hello, %s!\n", "world");
+** 	int rlen = printf("Hello, %s!\n", "world");
+** 	printf("len = %d, rlen = %d\n", len, rlen);
+**
+** 	len = ft_printf("Hello, %c%c%%!\n", 'w', '\0');
+** 	rlen = printf("Hello, %c%c%%!\n", 'w', '\0');
+** 	printf("len = %d, rlen = %d\n", len, rlen);
+**
+** 	len = ft_printf("Hello, %d, %d, %d, %d, %d!\n",
+** 		42, 0, 0, INT32_MAX, INT32_MIN);
+** 	rlen = printf("Hello, %d, %d, %d, %d, %d!\n",
+** 		42, 0, 0, INT32_MAX, INT32_MIN);
+** 	printf("len = %d, rlen = %d\n", len, rlen);
+**
+** 	len = ft_printf("Hello, %u, %u, %u, %u, %u!\n",
+** 		42, 0, 0, UINT32_MAX, UINT32_MAX + 1);
+** 	rlen = printf("Hello, %u, %u, %u, %u, %u!\n",
+** 		42, 0, 0, UINT32_MAX, UINT32_MAX + 1);
+** 	printf("len = %d, rlen = %d\n", len, rlen);
+**
+** 	len = ft_printf("Hello, %x, %x, %x, %x, %x!\n",
+** 		42, 0, 0, UINT32_MAX, UINT32_MAX + 1);
+** 	rlen = printf("Hello, %x, %x, %x, %x, %x!\n",
+** 		42, 0, 0, UINT32_MAX, UINT32_MAX + 1);
+** 	printf("len = %d, rlen = %d\n", len, rlen);
+**
+** 	len = ft_printf("Hello, %X, %X, %X, %X, %X!\n",
+** 		42, 0, 0, UINT32_MAX, UINT32_MAX + 1);
+** 	rlen = printf("Hello, %X, %X, %X, %X, %X!\n",
+** 		42, 0, 0, UINT32_MAX, UINT32_MAX + 1);
+** 	printf("len = %d, rlen = %d\n", len, rlen);
+**
+** 	len = ft_printf("Hello, %p, %p, %p!\n", &len, &rlen, NULL);
+** 	rlen = printf("Hello, %p, %p, %p!\n", &len, &rlen, NULL);
+** 	printf("len = %d, rlen = %d\n", len, rlen);
+** 	return (0);
+** }
+*/
