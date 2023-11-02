2dfb223d99aa7f1ef9908b2df35151582b773f5b
Torrenté Florian <florian@deneria.net>
2023-11-02 15:43:54 +0100

    Test

--- src/ft_nb_converters.c
+++ src/ft_nb_converters.c
@@ -6,7 +6,7 @@
 /*   By: fltorren <fltorren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 18:01:24 by fltorren          #+#    #+#             */
-/*   Updated: 2023/11/02 15:39:03 by fltorren         ###   ########.fr       */
+/*   Updated: 2023/11/02 15:43:30 by fltorren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,10 @@ static int	ft_put(char *str, int len, t_flags *flags)
 	if (flags->minus)
 		ft_write(str, len, *flags, neg);
 	if (!flags->zero || flags->minus)
-		ft_put_width(flags->width, ft_max(len, flags->precision));
+		ft_put_width(flags->width, ft_max(len, flags->precision) + (neg && flags->dot));
 	write(1, "-", neg && !flags->minus);
 	if (flags->zero && !flags->minus)
-		ft_put_zeroes(flags->width, ft_max(len, flags->precision));
+		ft_put_zeroes(flags->width, ft_max(len, flags->precision) + neg);
 	if (!flags->minus)
 		ft_write(str, len, *flags, neg);
 	free(str);
